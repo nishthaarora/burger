6e59b55353ff8d274c50c2cb3e747b216f40e589
Nishtha <nishtha@Nishthas-MacBook-Pro.local>
2016-11-09 11:37:13 -0600

    adding connections settings

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,22 @@
 // making sql connection
-var mysql      = require('mysql');
-var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : '',
-  database : 'burgers_db'
-});
+var mysql = require('mysql');
 
-connection.connect(function (err) {
+var connection;
+
+if (process.env.JAWSDB_URL) {
+	connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+	connection = mysql.createConnection({
+		host: 'localhost',
+		user: 'root',
+		password: '',
+		database: 'burgers_db'
+	});
+
+}
+
+
+connection.connect(function(err) {
 	if (err) {
 		console.error('error connecting: ' + err.stack);
 		return;
